1de840f7c9e5f73c04bcdf5281a148cb5947ba03
nabilbonie17-prog <nabilbonie17@gmail.com>
2026-03-28 03:54:22 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,8 @@ const playfair = Playfair_Display({
 });
 
 export const metadata: Metadata = {
-  title: "Happy Valentine L",
-  description: "A special Valentine's gift for L ❤️",
+  title: "Happy Birthday Azka Nur Kholifah",
+  description: "A special birthday gift for u ❤️",
 };
 
 export default function RootLayout({
